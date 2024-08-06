e09d6a1351cecc89ebeaa901afdf98ab2cd16d75
big-good-boy <good_boy@live.ru>
2024-08-06 11:22:19 +0500

    added the 25 todo app v2 project

---
+++ 25-todo-app-v2/src/App.js
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import './App.css';
+import TodoForm from './components/Todos/TodoForm';
+import TodoList from './components/Todos/TodoList';
+
+function App() {
+	const [todos, setTodos] = useState([]);
+
+	const addTodoHandler = (text) => {
+		const newTodo = {
+			text,
+			isCompleted: false,
+			id: uuidv4(),
+		};
+
+		setTodos([...todos, newTodo]);
+	};
+
+	const deleteTodoHandler = (id) => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
+	const toggleTodoHandler = (id) => {
+		setTodos(
+			todos.map((todo) =>
+				todo.id === id
+					? { ...todo, isCompleted: !todo.isCompleted }
+					: { ...todo }
+			)
+		);
+	};
+
+	return (
+		<div className="App">
+			<h1>Todo App</h1>
+			<TodoForm addTodo={addTodoHandler} />
+			<TodoList
+				todos={todos}
+				deleteTodo={deleteTodoHandler}
+				toggleTodo={toggleTodoHandler}
+			/>
+		</div>
+	);
+}
+
+export default App;
