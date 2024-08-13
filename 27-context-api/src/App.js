7e886f462acf333048156e64cbb8f1764180686e
big-good-boy <good_boy@live.ru>
2024-08-13 11:01:55 +0500

    added the 27 context api project

---
+++ 27-context-api/src/App.js
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import UserContext from './context/UserContext';
+import User from './components/User';
+import './App.css';
+
+function App() {
+	const [user, setUser] = useState('Bogdan');
+	return (
+		<UserContext.Provider value={user}>
+			<div className="App">
+				<User />
+			</div>
+		</UserContext.Provider>
+	);
+}
+
+export default App;
